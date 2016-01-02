6dd3d76c7f72bf34d5bb8f513872377acf1bafd8
MinhHuy1234 <MinhHuy1234@ymail.com>
2016-01-02 11:11:54 -0500

    Addes Game Introdution
    Fixes #3

---
+++ Text Based Game/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout<<"HEY, you, I'm alive! Oh, and Hello World!\n";
+  cin.get();
+  cout<<"Either I rule this world or it will not exist!!!\n";
+  cin.get();
+}
