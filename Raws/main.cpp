9c1e84f8d9e1c86b5ab92793c34c9d86de682b85
MinhHuy1234 <MinhHuy1234@ymail.com>
2016-01-09 14:12:08 -0500

    Bugfixes and code Cleaning
    Adds some variables and Fixes some bugs.

--- Raws/main.cpp
+++ Raws/main.cpp
@@ -2,19 +2,29 @@
 
 using namespace std;
 
+
 int main()
 {
-    int TrueOrFalse;
+    int Q1;
   cout<<"HEY, I'm alive! Oh, Hello World!\n";
   cin.get();
   cout<<"I need your input.\n";
   cin.get();
-  cout<<"1 is true and 0 is false.";
-  cin>> TrueOrFalse;
+  cout<<"1 is true and 0 is false.\n";//Next section will determine if true or false.
+  cin>> Q1;
   cin.ignore();
-  if (!(TrueOrFalse == 1||0))
-    {
-        cout<<"The input">>TrueOrFalse "is not recognized";
-    }
+  if (Q1!=0&&Q1!=1){
+    cout<<"The input, "<<Q1;
+    cout<<", is invalid.\n";
+    cin.get();
+  }
+  else if (Q1==0){
+    cout<<"This input is false.\n";
+    cin.get();
+  }
+  else {
+    cout<<"The input is true.\n";
+    cin.get();
+  }
 
   }
